a2486ffb427ad876f7bb1654de36f6239c9aa030
lakhan <oplopr01@gmail.com>
2024-02-16 20:30:43 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navbar from './components/Navbar/Navbar'
 import Greeting from './containers/Greeting/Greeting'
 import About from './containers/About/About'
-import Project from './containers/Project/Project'
+// import Project from './containers/Project/Project'
 import Contact from './containers/Contact/Contact'
 import Footer from './containers/Footer/Footer'
 import Education from './containers/Education/Education'
-import Coding from './containers/coding/Coding'
+// import Coding from './containers/coding/Coding'
 import {Link} from 'react-router-dom'
 import './App.css';
 import Experience from './containers/Experience/Experience'
