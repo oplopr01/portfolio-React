3f773dde63942df056f9c2f823a6a4d7b1f003c2
oplopr01 <oplopr01@gmail.com>
2026-02-17 00:37:10 +0530

    updated details

--- src/containers/Greeting/Greeting.jsx
+++ src/containers/Greeting/Greeting.jsx
@@ -20,19 +20,23 @@ const Greeting = () => {
                 <div className="home-content">
                     <div className="text-1">{greeting.title}</div>
                     <div className="text-2">{greeting.username}</div>
-                    <div className="text-3"> <p>As a recent graduate in Information Technology (2022) and trained on fullstack development, I bring a versatile skill set and a passion for technology. I am eager to embark on my journey in the field of software development or full-stack web development. With a solid foundation in engineering and a diverse range of skills, I am enthusiastic about securing an entry-level position where I can continue to learn, grow, and make meaningful contributions.</p>  </div>
+                    <div className="text-3"> <p>React Developer with 2 years of professional experience in building scalable, user-focused web
+                        applications within enterprise environments. Strong hands-on experience with React,
+                        TypeScript, JavaScript, and modern frontend tooling, along with exposure to backend
+                        integration using REST APIs and GraphQL. Proven ability to work in Agile teams, collaborate with
+                        Backend, QA teams and cross-functional stakeholders, and deliver high-quality features.</p>  </div>
                     <div className="resume">
                         <a href={process.env.PUBLIC_URL + `LakhanRathore_FullStack_0.pdf`} target="_blank" rel="noreferrer">See My Resume</a>
-                    </div>  
+                    </div>
                     <div className="profileLinks">
                         <a href="https://www.linkedin.com/in/lakhan-rathore-b88796188" target="_blank" rel="noreferrer">LinkedIn</a>
                         <a href="https://github.com/oplopr01" target="_blank" rel="noreferrer">GitHub</a>
                     </div>
-                        
+
                 </div>
-                    <div className="homeImageContainer">
-                        <img className='homeImage' src={profile} alt="" />
-                    </div>
+                <div className="homeImageContainer">
+                    <img className='homeImage' src={profile} alt="" />
+                </div>
             </div>
         </section>
     )
